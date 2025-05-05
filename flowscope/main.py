83ff9589973a7be81a539b84cb8768cb02ffc095
ccf20 <ccf20@ic.ac.uk>
2025-05-05 15:30:00 +0100

    [Style] pretty printing instructions

--- flowscope/main.py
+++ flowscope/main.py
@@ -4,11 +4,13 @@ from parser import Parser
 from interpreter import Environment
 from interpreter import Executor
 from lexer import Lexer
+from pcfgbuilder import PCFGBuilder
 
 def main():
     argparser = argparse.ArgumentParser(description="Run interpreter on input file.")
     argparser.add_argument("filename", help="Path to the .i file to run.")
     argparser.add_argument("--seed", type=int, help="Optional random seed for reproducibility.")
+    argparser.add_argument("--pcfg", action="store_true", help="Optional to generate a PCF")
 
     args = argparser.parse_args() 
 
@@ -32,16 +34,21 @@ def main():
     executor = Executor(env, seed=args.seed)
     executor.run()
 
-    for name, info in variables.items():
-        print(f"{name}: {info}")
-    
-    for instr in instructions:
-        print(instr.__class__.__name__, vars(instr))
+    # for name, info in variables.items():
+        # print(f"{name}: {info}")
+    # 
+    # for instr in instructions:
+        # print(instr)
 
     print("Final memory state:")
 
     for k, v in env.memory.items():
         print(f"{k}: {v}")
+
+    if args.pcfg:
+        cfg = PCFGBuilder(instructions, env.labels)
+        cfg.build()
+        cfg.visualize("pcfg.png")
 
 if __name__ == "__main__":
     main()
